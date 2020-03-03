c50bdd831a00acb926aa43a6a840ea1993aa01b7
Olivier <rodomond.o@gmail.com>
2020-03-03 09:01:13 +0700

    add broker service

---
+++ server/services/broker.js
@@ -0,0 +1,24 @@
+const MQTT = require('mqtt')
+ 
+let globalOptions = (global.options && global.options.mqtt) || {}
+
+module.exports = {
+  client: MQTT.connect(`mqtt://${globalOptions.host}`),
+  options: {
+    properties: {
+      contentType: 'application/json',
+      messageExpiryInterval: 3600,
+    }
+  },
+  publish: function(msg) {
+    try {
+      if (this.client.connected) {
+        this.client.publish(globalOptions.topic, JSON.stringify(msg), this.options)
+      } else {
+        console.log('Broker is not connected')
+      }
+    } catch(e) {
+      console.log(e)
+    }
+  }
+}
---
+++ server/services/broker.test.js
@@ -0,0 +1,119 @@
+beforeEach(() => {
+  jest.resetModules()
+  delete global.options
+  delete global.console
+})
+
+describe('#Services - broker', () => {
+  test('Initial properties', () => {
+    global.options = {
+      mqtt: {
+        host: 'test.com'
+      }
+    }
+    let MQTT = require('mqtt')
+    jest.mock('mqtt')
+    
+    MQTT.connect = jest.fn()
+
+    let Broker = require('./broker')
+
+    expect(Object.keys(Broker)).toEqual(['client', 'options', 'publish'])
+    expect(Broker.options).toEqual({
+      properties: {
+        contentType: 'application/json',
+        messageExpiryInterval: 3600,
+      }
+    })
+    expect(MQTT.connect.mock.calls.length).toBe(1)
+    expect(MQTT.connect.mock.calls[0][0]).toBe('mqtt://test.com')
+  })
+
+  describe('#publish', () => {
+    test('if the client is not connected', () => {
+      global.options = {
+        mqtt: {
+          topic: 'topic/test'
+        }
+      }
+      global.console = {
+        log: jest.fn()
+      }
+      
+      let client = {
+        connected: false,
+        publish: jest.fn()
+      }
+
+      let Broker = require('./broker')
+      Broker.client = client
+
+      Broker.publish({})
+
+      expect(Broker.client.publish.mock.calls.length).toBe(0)
+      expect(global.console.log.mock.calls.length).toBe(1)
+      expect(global.console.log.mock.calls[0][0]).toBe('Broker is not connected')
+    })
+
+    test('if the client is connected', () => {
+      global.options = {
+        mqtt: {
+          topic: 'topic/test'
+        }
+      }
+      global.console = {
+        log: jest.fn()
+      }
+      
+      let client = {
+        connected: true,
+        publish: jest.fn()
+      }
+
+      let Broker = require('./broker')
+      Broker.client = client
+      Broker.options = {
+        foo: 'bar'
+      }
+
+      Broker.publish({hello: 'world'})
+
+      expect(Broker.client.publish.mock.calls.length).toBe(1)
+      expect(Broker.client.publish.mock.calls[0][0]).toEqual('topic/test')
+      expect(Broker.client.publish.mock.calls[0][1]).toEqual(JSON.stringify({'hello': 'world'}))
+      expect(Broker.client.publish.mock.calls[0][2]).toEqual(Broker.options)
+      expect(global.console.log.mock.calls.length).toBe(0)
+    })
+
+    test('if it throws an error', () => {
+      global.options = {
+        mqtt: {
+          topic: 'topic/test'
+        }
+      }
+      global.console = {
+        log: jest.fn()
+      }
+      
+      let client = {
+        connected: true,
+        publish: jest.fn(() => {
+          throw 'My Error'
+        })
+      }
+
+      let Broker = require('./broker')
+      Broker.client = client
+      Broker.options = {
+        foo: 'bar'
+      }
+
+      Broker.publish({hello: 'world'})
+
+      expect(Broker.client.publish.mock.calls.length).toBe(1)
+      expect(global.console.log.mock.calls.length).toBe(1)
+      expect(global.console.log.mock.calls[0][0]).toBe('My Error')
+    })
+  })
+})
+
