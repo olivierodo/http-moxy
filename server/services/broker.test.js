504e14ac62519400c907b10b3c1b0305843ff077
Olivier <rodomond.o@gmail.com>
2020-03-09 08:36:41 +0700

    add tests

--- server/services/broker.test.js
+++ server/services/broker.test.js
@@ -11,18 +11,18 @@ describe('#Services - broker', () => {
         host: 'test.com'
       }
     }
-    let MQTT = require('mqtt')
+    const MQTT = require('mqtt')
     jest.mock('mqtt')
-    
+
     MQTT.connect = jest.fn()
 
-    let Broker = require('./broker')
+    const Broker = require('./broker')
 
     expect(Object.keys(Broker)).toEqual(['client', 'options', 'publish'])
     expect(Broker.options).toEqual({
       properties: {
         contentType: 'application/json',
-        messageExpiryInterval: 3600,
+        messageExpiryInterval: 3600
       }
     })
     expect(MQTT.connect.mock.calls.length).toBe(1)
@@ -39,13 +39,13 @@ describe('#Services - broker', () => {
       global.console = {
         log: jest.fn()
       }
-      
-      let client = {
+
+      const client = {
         connected: false,
         publish: jest.fn()
       }
 
-      let Broker = require('./broker')
+      const Broker = require('./broker')
       Broker.client = client
 
       Broker.publish({})
@@ -64,23 +64,23 @@ describe('#Services - broker', () => {
       global.console = {
         log: jest.fn()
       }
-      
-      let client = {
+
+      const client = {
         connected: true,
         publish: jest.fn()
       }
 
-      let Broker = require('./broker')
+      const Broker = require('./broker')
       Broker.client = client
       Broker.options = {
         foo: 'bar'
       }
 
-      Broker.publish({hello: 'world'})
+      Broker.publish({ hello: 'world' })
 
       expect(Broker.client.publish.mock.calls.length).toBe(1)
       expect(Broker.client.publish.mock.calls[0][0]).toEqual('topic/test')
-      expect(Broker.client.publish.mock.calls[0][1]).toEqual(JSON.stringify({'hello': 'world'}))
+      expect(Broker.client.publish.mock.calls[0][1]).toEqual(JSON.stringify({ hello: 'world' }))
       expect(Broker.client.publish.mock.calls[0][2]).toEqual(Broker.options)
       expect(global.console.log.mock.calls.length).toBe(0)
     })
@@ -94,26 +94,26 @@ describe('#Services - broker', () => {
       global.console = {
         log: jest.fn()
       }
-      
-      let client = {
+
+      const client = {
         connected: true,
         publish: jest.fn(() => {
-          throw 'My Error'
+          throw new Error('My Error')
         })
       }
 
-      let Broker = require('./broker')
+      const Broker = require('./broker')
       Broker.client = client
       Broker.options = {
         foo: 'bar'
       }
 
-      Broker.publish({hello: 'world'})
+      Broker.publish({ hello: 'world' })
 
       expect(Broker.client.publish.mock.calls.length).toBe(1)
       expect(global.console.log.mock.calls.length).toBe(1)
-      expect(global.console.log.mock.calls[0][0]).toBe('My Error')
+      expect(global.console.log.mock.calls[0][0]).toBeInstanceOf(Error)
+      expect(global.console.log.mock.calls[0][0].message).toBe('My Error')
     })
   })
 })
-
